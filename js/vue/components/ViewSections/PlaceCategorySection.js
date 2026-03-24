ca168be28db11bc5650938906a1faf7e2602d308
Hamada <ahmedessamskills@gmail.com>
2026-03-24 22:44:47 +0400

    Remove stray text from PlaceCategorySection component template

--- js/vue/components/ViewSections/PlaceCategorySection.js
+++ js/vue/components/ViewSections/PlaceCategorySection.js
@@ -34,7 +34,7 @@ const PlaceCategorySection = {
         <div class="absolute inset-0 bg-black/30"></div>
 
         <!-- Content -->
-        <div class="relative z-10 text-center">duration-300
+        <div class="relative z-10 text-center">
             <div class="text-[6em] font-bold text-white mb-8 drop-shadow-lg">{{ title }}</div>
             <a :href="link" class="btn btn-outline btn-lg rounded-full px-[70px] py-[14px] text-white border-white hover:bg-white hover:text-black transition-all duration-300">
                 {{ buttonText }}
